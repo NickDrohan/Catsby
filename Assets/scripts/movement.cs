41f0b93ff996d281004423fe10bb342bba53d94e
Lucy Ungaro <lucyu12@gmail.com>
2016-01-11 14:51:23 -0500

    test stuff

---
+++ Assets/scripts/movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+	
+	public float Speed = 0f;
+	private float movex = 0f;
+	//private float movey = 0f;
+	Rigidbody rbody; 
+	
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody> (); 
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			transform.position += new Vector3(0f, 4f, 0f); 
+		}
+		
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		movex = Input.GetAxis ("Horizontal");
+		//movey = Input.GetAxis ("Vertical");
+		rbody.velocity = new Vector3 (movex * Speed, 0f, 0f);
+	}
+}
+
