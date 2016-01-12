9eff2bd48bc321e785e1ad0d9af25883c487bd76
Lucy Ungaro <lucyu12@gmail.com>
2016-01-11 23:56:14 -0500

    scene transitions

---
+++ Assets/scripts/sceneTransition.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sceneTransition : MonoBehaviour {
+	public Transform marker; 
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+		if ((marker.position - transform.position).magnitude < 5f && Input.GetKeyDown (KeyCode.E)) {
+			Application.LoadLevel ("alley"); 
+
+		}
+	}
+}
